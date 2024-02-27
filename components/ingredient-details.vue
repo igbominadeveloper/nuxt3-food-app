b704f81f85fb12b4eb99295202e7e1a7468fb8e2
Favour Afolayan <favourafolayan@gmail.com>
2024-02-27 13:46:17 +0100

    Add popover component to recipe ingredients

--- components/ingredient-details.vue
+++ components/ingredient-details.vue
@@ -9,18 +9,33 @@ defineProps<{
 </script>
 
 <template>
-  <article class="w-48 shadow-md p-2 text-black dark:text-white text-sm">
-    <div>
-      <h3 class="font-normal">Name</h3>
-      <p>{{ ingredient.name }}</p>
+  <article
+    class="min-w-80 shadow-md p-2 text-black dark:text-white text-sm flex flex-col"
+  >
+    <div class="h-60 w-[45%] max-w-40 mx-auto">
+      <BaseImage
+        provider="spoonacular"
+        class="object-contain h-full w-full"
+        :image-url="ingredient.image!"
+      />
     </div>
 
-    <div>
-      <NuxtImg provider="spoonacular" :src="ingredient.image!" />
-    </div>
-    <div>
-      <h3 class="font-normal">Name</h3>
+    <row direction="row">
+      <h3 class="font-normal">Name:</h3>
       <p>{{ ingredient.name }}</p>
-    </div>
+    </row>
+
+    <row direction="row">
+      <h3 class="font-normal">Measure:</h3>
+      <p>
+        {{ ingredient.measures[measure].amount }}
+        {{ ingredient.measures[measure].unitLong }}
+      </p>
+    </row>
+
+    <row direction="row">
+      <h3 class="font-normal">Aisle:</h3>
+      <p>{{ ingredient.aisle }}</p>
+    </row>
   </article>
 </template>
